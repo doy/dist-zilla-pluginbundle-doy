11217d64201930725841f30dd659ca94adc1108a
Jesse Luehrs <doy@tozt.net>
2011-02-16 11:13:18 -0600

    stop using the template thing

--- lib/Dist/Zilla/PluginBundle/DOY.pm
+++ lib/Dist/Zilla/PluginBundle/DOY.pm
@@ -185,10 +185,46 @@ sub configure {
     );
 }
 
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-dist-zilla-pluginbundle-doy at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dist-Zilla-PluginBundle-DOY>.
+
 =head1 SEE ALSO
 
 L<Dist::Zilla>
 L<Task::BeLike::DOY>
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc Dist::Zilla::PluginBundle::DOY
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Dist-Zilla-PluginBundle-DOY>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Dist-Zilla-PluginBundle-DOY>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dist-Zilla-PluginBundle-DOY>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Dist-Zilla-PluginBundle-DOY>
+
+=back
 
 =begin Pod::Coverage
 
